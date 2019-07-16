55293823d5926885b668239f42cb505cb9740614
François Pluchino <francois.pluchino@gmail.com>
2019-07-16 18:48:29 +0200

    Add class finder

---
+++ Tests/Loader/ClassFinderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Fxp package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fxp\Component\Config\Tests\Loader;
+
+use Fxp\Component\Config\Loader\ClassFinder;
+use Fxp\Component\Config\Tests\Fixtures\Model\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ *
+ * @internal
+ */
+final class ClassFinderTest extends TestCase
+{
+    public function testFindClasses(): void
+    {
+        $finder = new ClassFinder(
+            [
+                \dirname(__DIR__).'/Fixtures',
+            ],
+            [
+                'Foo',
+            ]
+        );
+
+        $expected = [
+            MockObject::class,
+        ];
+
+        static::assertSame($expected, $finder->findClasses());
+    }
+}
